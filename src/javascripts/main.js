4a298f159252d759579f121e11ef2ac63aadf752
delindell <daviselindell@gmail.com>
2020-03-07 12:37:03 -0600

    filtering buttons work

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,12 +1,14 @@
 import navbar from './components/navbar/navbar';
 import studentList from './components/studentList/studentList';
 import studentData from './helpers/data/studentData';
+import houseButtonGroup from './components/houseButtonGroup/houseButtonGroup';
 import '../styles/main.scss';
 
 const init = () => {
   const allStudents = studentData.getStudents();
   navbar.loadNavbar();
   studentList.createStudentList(allStudents);
+  houseButtonGroup.createHouseButtonGroup();
 };
 
 init();
