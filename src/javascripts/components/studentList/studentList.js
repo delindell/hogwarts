fab3c38d35d682ae20dd147bdcd84a2b20c2c31a
delindell <daviselindell@gmail.com>
2020-03-07 11:07:34 -0600

    student card displaying

--- src/javascripts/components/studentList/studentList.js
+++ src/javascripts/components/studentList/studentList.js
@@ -1,10 +1,11 @@
 import './studentList.scss';
 import utils from '../../helpers/utils';
+import studentCard from '../studentCard/studentCard';
 
 const createStudentList = (students) => {
   let domString = '<ul class="student-list">';
   students.forEach((student) => {
-    domString += `${student.name}`;
+    domString += studentCard.createStudentCard(student);
   });
   domString += '</ul>';
   utils.printToDom('student-container', domString);
